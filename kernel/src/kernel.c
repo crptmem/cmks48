525087e16338600484c3bafd3016e7398a1aff3e
corruptmemry <88046785+corruptmemry@users.noreply.github.com>
2021-08-15 22:29:58 +0700

    Add files via upload

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,4 +1,10 @@
+#include <video/gop.h>
 
-int _start(){
-    return 123;
+void _start(Framebuffer* framebuffer, PSF1_FONT* psf1_font){
+    CursorPosition.X = 10;
+    CursorPosition.Y = 10;
+    
+    Print(framebuffer,psf1_font, 0xFFFFFF, "AirKernel 0.1-indev");
+    kprint(" - nothing to do...", 0xFFFFFF, "_start - kernel.c");
+    return ;
 }
