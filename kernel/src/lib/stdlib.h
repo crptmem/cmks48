ff71fd97e088dbbf9d0674030eaea801a3d8ab02
TheAirBlow <pilexet.danila@gmail.com>
2021-08-16 17:51:51 +0500

    dev fix

---
+++ kernel/src/lib/stdlib.h
@@ -0,0 +1,5 @@
+#include "types.h"
+
+extern char *strcpy(char *dst, const char *src);
+extern char *strcat(char *dst, const char *src);
+extern size_t strlen(const char* str);
---
+++ kernel/src/video/gop.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <lib/types.h>
+typedef struct {
+	void* BaseAddress;
+	size_t BufferSize;
+	unsigned int Width;
+	unsigned int Height;
+	unsigned int PixelsPerScanLine;
+} Framebuffer;
+
+typedef struct {
+	unsigned char magic[2];
+	unsigned char mode;
+	unsigned char charsize;
+} PSF1_HEADER;
+
+typedef struct {
+	PSF1_HEADER* psf1_Header;
+	void* glyphBuffer;
+} PSF1_FONT;
+
+typedef struct {
+    unsigned int X;
+    unsigned int Y;
+} Point;
+
+extern Point CursorPosition;
+extern void Print(Framebuffer* framebuffer, PSF1_FONT* psf1_font, unsigned int colour, char* str);
+extern void Clear();
+extern void kprint(char* str[], unsigned int color,  const char* caller);
+extern void newline();
+extern void PutChar(char* chr, unsigned int colour, unsigned int x, unsigned int y);
