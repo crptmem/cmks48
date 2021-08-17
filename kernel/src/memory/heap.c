487023f67d3a2474b59edf733bfe327153744362
corruptmemry <88046785+corruptmemry@users.noreply.github.com>
2021-08-17 23:02:51 +0700

    Update heap.c

--- kernel/src/memory/heap.c
+++ kernel/src/memory/heap.c
@@ -3,12 +3,6 @@
 #include <string.h>
 #include <memory/heap.h>
 
-void* memset(void* bufptr, int value, size_t size) {
-	unsigned char* buf = (unsigned char*) bufptr;
-	for (size_t i = 0; i < size; i++)
-		buf[i] = (unsigned char) value;
-	return bufptr;
-}
 #define MAX_PAGE_ALIGNED_ALLOCS 32
 
 uint32_t last_alloc = 0;
@@ -36,41 +30,26 @@ void mm_init(uint32_t kernel_end)
 char* malloc(size_t size)
 {
 	if(!size) return 0;
-
-	/* Loop through blocks and find a block sized the same or bigger */
 	uint8_t *mem = (uint8_t *)heap_begin;
 	while((uint32_t)mem < last_alloc)
 	{
 		alloc_t *a = (alloc_t *)mem;
-		/* If the alloc has no size, we have reaced the end of allocation */
-		//mprint("mem=0x%x a={.status=%d, .size=%d}\n", mem, a->status, a->size);
 		if(!a->size)
 			goto nalloc;
-		/* If the alloc has a status of 1 (allocated), then add its size
-		 * and the sizeof alloc_t to the memory and continue looking.
-		 */
 		if(a->status) {
 			mem += a->size;
 			mem += sizeof(alloc_t);
 			mem += 4;
 			continue;
 		}
-		/* If the is not allocated, and its size is bigger or equal to the
-		 * requested size, then adjust its size, set status and return the location.
-		 */
 		if(a->size >= size)
 		{
-			/* Set to allocated */
 			a->status = 1;
 
 			memset(mem + sizeof(alloc_t), 0, size);
 			memory_used += size + sizeof(alloc_t);
 			return (char *)(mem + sizeof(alloc_t));
 		}
-		/* If it isn't allocated, but the size is not good, then
-		 * add its size and the sizeof alloc_t to the pointer and
-		 * continue;
-		 */
 		mem += a->size;
 		mem += sizeof(alloc_t);
 		mem += 4;
@@ -84,23 +63,13 @@ char* malloc(size_t size)
 	last_alloc += size;
 	last_alloc += sizeof(alloc_t);
 	last_alloc += 4;
-    kprint("Allocated ", 0xFFFFFF);
-    kprint(to_string64(size), 0xFFFFFF);
-    kprint(" bytes from 0x", 0xFFFFFF);
-    kprint(to_hstring32((uint32_t)alloc + sizeof(alloc_t)), 0xFFFFFF);
-    kprint(" to 0x", 0xFFFFFF);
-    kprint(to_hstring32(last_alloc), 0xFFFFFF);
+   	kprint("Allocated ", 0xFFFFFF);
+   	kprint(to_string64(size), 0xFFFFFF);
+   	kprint(" bytes from 0x", 0xFFFFFF);
+   	kprint(to_hstring32((uint32_t)alloc + sizeof(alloc_t)), 0xFFFFFF);
+   	kprint(" to 0x", 0xFFFFFF);
+   	kprint(to_hstring32(last_alloc), 0xFFFFFF);
 	memory_used += size + 4 + sizeof(alloc_t);
 	memset((char *)((uint32_t)alloc + sizeof(alloc_t)), 0, size);
 	return (char *)((uint32_t)alloc + sizeof(alloc_t));
-/*
-	char* ret = (char*)last_alloc;
-	last_alloc += size;
-	if(last_alloc >= heap_end)
-	{
-		set_task(0);
-		panic("Cannot allocate %d bytes! Out of memory.\n", size);
-	}
-	mprint("Allocated %d bytes from 0x%x to 0x%x\n", size, ret, last_alloc);
-	return ret;*/
 }
