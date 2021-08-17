5dcb522b8251ef3fdd4225166afdbc43132574e7
corruptmemry <88046785+corruptmemry@users.noreply.github.com>
2021-08-17 23:03:08 +0700

    Update heap.h

--- kernel/src/memory/heap.h
+++ kernel/src/memory/heap.h
@@ -9,13 +9,8 @@ typedef struct {
 } alloc_t;
 
 extern void mm_init(uint32_t kernel_end);
-extern void mm_print_out();
 
-extern void paging_init();
-extern void paging_map_virtual_to_phys(uint32_t virt, uint32_t phys);
-
-extern char* pmalloc(size_t size); /* page aligned alloc */
 extern char* malloc(size_t size);
-extern void free(void *mem);
+extern void free(void *mem); /* will add soon */
 
 #endif
